6709da16e07ae6cef985c146c74e41fbb443be8c
Edmer Valencia <edmer_franciz@yahoo.com>
2022-05-29 01:21:43 -0500

    added initial schemas

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,28 @@
 DROP TABLE IF EXISTS  department;   
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
 
+--department schema
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+--role schema
+CREATE TABLE role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+--employee schema
+CREATE TABLE employee (
+    id
+    first_name
+    last_name
+    role_id
+    manager_id 
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+)
 
-
-department
-
-id: INT PRIMARY KEY
-
-name: VARCHAR(30) to hold department name
-
-role
-
-id: INT PRIMARY KEY
-
-title: VARCHAR(30) to hold role title
-
-salary: DECIMAL to hold role salary
-
-department_id: INT to hold reference to department role belongs to
-
-employee
-
-id: INT PRIMARY KEY
-
-first_name: VARCHAR(30) to hold employee first name
-
-last_name: VARCHAR(30) to hold employee last name
-
-role_id: INT to hold reference to employee role
-
-manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
